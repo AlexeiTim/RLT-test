061803f5bbfbdfeffc8f3818e55e8c912f214bbd
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-08-02 01:05:04 +0300

    нашел утилиту для генерации цветов  с определенной разницей

--- src/lib/generateColors.ts
+++ src/lib/generateColors.ts
@@ -1,13 +1,21 @@
-export function generateTwoColors(): [string, string] {
-  const r = Math.floor(Math.random() * 256)
-  const g = Math.floor(Math.random() * 256)
-  const b = Math.floor(Math.random() * 256)
+function generateColorWithAlpha(r: number, g: number, b: number, alpha: number) {
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`
+}
+
+export function generateTwoColors(diff = 50): [string, string] {
+  const r1 = Math.floor(Math.random() * 256)
+  const g1 = Math.floor(Math.random() * 256)
+  const b1 = Math.floor(Math.random() * 256)
+
+  const r2 = Math.min(Math.max(r1 + Math.floor(Math.random() * (diff * 2)) - diff, 0), 255)
+  const g2 = Math.min(Math.max(g1 + Math.floor(Math.random() * (diff * 2)) - diff, 0), 255)
+  const b2 = Math.min(Math.max(b1 + Math.floor(Math.random() * (diff * 2)) - diff, 0), 255)
 
   const alpha1 = 1
   const alpha2 = 0.35
 
-  const color1 = `rgba(${r}, ${g}, ${b}, ${alpha1})`
-  const color2 = `rgba(${r}, ${g}, ${b}, ${alpha2})`
+  const color1 = generateColorWithAlpha(r1, g1, b1, alpha1)
+  const color2 = generateColorWithAlpha(r2, g2, b2, alpha2)
 
   return [color1, color2]
 }
